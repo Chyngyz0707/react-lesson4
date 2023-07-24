f7c55851a46daf6f23885740bd1ec452bc9ee7ec
Chyngyz1707 <Chyngyz.baitemirov@mail.ru>
2023-07-25 00:11:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+// Неконтролируемый компонент
+export default class Form extends React.Component {
+  handleSubmitClick = () =>{
+    const name = this._name.value;
+  };
+  render() {
+    return {
+      <div>
+        <input type="text" ref={(input) => (this._name = input)} />
+        <button onClick={this.handleSubmitClick}>Sign Up!!</button>
+      
+      </div>
+        
+    };
+  }
+} 
---
+++ src/Form.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class KeyForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+        };
+    }
+    handleNameChange = (event) => {
+        this.setState({ name:event.target.value});
+    }; 
+
+    render() {
+        return (
+            <div>
+                <input
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                />
+            </div>
+        );
+    }
+
+}
